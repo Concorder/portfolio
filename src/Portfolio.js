54cd2e311bf6e1d7f64bae193a07ff2dc064604f
Concorder <56825826+Concorder@users.noreply.github.com>
2023-10-26 06:16:48 +0300

    compression of images

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,15 +3,17 @@ import Workpreview from "./components/Workpreview";
 import radinetImg from "./img/portfolio/radinet.jpg"
 import mindsBoomImg from "./img/portfolio/mindsboom.jpg"
 import iLandImg from "./img/portfolio/landing1.jpg"
+import retroforex from "./img/portfolio/retroforex.jpg"
 import iphone3d from "./img/portfolio/iPhone3d.jpg"
 import brainImg from "./img/portfolio/brainstove.jpg"
-import oksiImg from "./img/portfolio/oksi.jpg"
+// import oksiImg from "./img/portfolio/oksi.jpg"
 const works = [
     {name:"Arya Mg - Corp. Site",image: radinetImg,link:"https://concorder.github.io/Arya/"},
-    {name:"Portal (in progress)",image: oksiImg,link:"https://www.oksigames.com/"},
+    // {name:"HTML Games portal",image: oksiImg,link:"https://www.oksigames.com/"},
     {name:"Gaming Portal",image: brainImg,link:"https://brainstove.com/"},
     {name:"Recipes Portal",image: mindsBoomImg,link:"https://mindsboom.com/"},
     {name:"iPhone Landing",image: iLandImg,link:"https://concorder.github.io/pf/iphone_pl_1.html"},
+    {name:"Retro FX",image: retroforex,link:"https://concorder.github.io/pf/retroforex.html"},
     {name:"iPhone 3d",image: iphone3d,link:"https://concorder.github.io/pf/iPhone3d.html"},
     ]
 
